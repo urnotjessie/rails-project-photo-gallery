1ac5eaaa8c226b2a873493ed41a1c993a89fe402
urnotjessie <zg2220@columbia.edu>
2018-08-23 22:43:20 -0400

    basic photos_controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,24 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption, :image)
+  end
+end
