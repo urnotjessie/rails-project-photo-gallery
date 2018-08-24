9d6b6092f3c385f7db7d3e709aeb913528c45d6a
urnotjessie <zg2220@columbia.edu>
2018-08-24 11:21:28 -0400

    test activestorage upload images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,8 @@
 class Photo < ApplicationRecord
-  has_one_attached :image
-  
+
   belongs_to :creator, :class_name => "User"
   has_many :photo_users
   has_many :collectors, through: :photo_users, class_name: 'User', foreign_key: :collector_id
+
+  has_one_attached :image
 end
