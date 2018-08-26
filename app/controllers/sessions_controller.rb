42e81bbc4fc5e34364657bf30deef7f01fa139e7
urnotjessie <zg2220@columbia.edu>
2018-08-25 23:53:22 -0400

    omniauth working and set up welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,39 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    
-    if @user && @user.authenticate(params[:password])
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.username = auth['info']['name']
+        u.email = auth['info']['email']
+        u.password = SecureRandom.hex
+      end
       session[:user_id] = @user.id
       redirect_to @user
+    else
+      @user = User.find_by(email: params[:email])
+
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        redirect_to '/'
+      end
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session.delete :user_id
+      redirect_to '/'
     else
       redirect_to '/'
     end
   end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_200543) do
+ActiveRecord::Schema.define(version: 2018_08_25_200608) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_08_24_200543) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
   end
 
 end
