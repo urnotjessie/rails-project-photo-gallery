c89cde0e181558cb621cf8805875ffa74d105d79
urnotjessie <zg2220@columbia.edu>
2018-08-30 20:26:29 -0400

    dynamic layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  layout :determine_layout
+
   def current_user
     User.find(session[:user_id])
   end
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def determine_layout
+    if logged_in?
+      "logged_in"
+    else
+      "application"
+    end
+  end
+
 end
