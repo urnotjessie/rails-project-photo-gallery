cff1a7f5f6bbd2a3f81f6284372dfe2037043b06
urnotjessie <zg2220@columbia.edu>
2018-08-31 23:09:33 -0400

    implement user profile update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
 
   private
 
