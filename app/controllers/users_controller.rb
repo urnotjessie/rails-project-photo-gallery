3693e2c148646aea340439a1f26a59fec763accf
urnotjessie <zg2220@columbia.edu>
2018-10-19 11:12:23 -0400

    set up Active Model Serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @user }
+    end
   end
 
   def edit
