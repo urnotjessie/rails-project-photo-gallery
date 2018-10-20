231f3c9773fbda45dfca22434e0b4d401adc5500
urnotjessie <zg2220@columbia.edu>
2018-10-20 18:20:39 -0400

    customize has_many photos attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
-  has_many :photos
+  attributes :id, :username, :photos
+
+  has_many :collected_photos, through: :photo_users, serializer: PhotoSerializer
+
+  def photos
+    user_photos = []
+
+    object.photos.each do |photo|
+      user_photo = photo.attributes
+      user_photo[:collectors] = photo.collectors.collect{|collect| collect.slice(:id, :username)}
+      user_photos.push(user_photo)
+    end
+
+    return user_photos
+    
+  end
 end
