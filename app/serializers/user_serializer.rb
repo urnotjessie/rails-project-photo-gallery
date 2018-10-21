efd52c192611d2b4b0c5db2b66c8c755067c4865
urnotjessie <zg2220@columbia.edu>
2018-10-21 10:21:22 -0400

    serialized attached image url to json

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :username, :photos
 
   has_many :collected_photos, through: :photo_users, serializer: PhotoSerializer
@@ -8,11 +9,15 @@ class UserSerializer < ActiveModel::Serializer
 
     object.photos.each do |photo|
       user_photo = photo.attributes
+
+      thumbnail = photo.image.variant(resize:'300x300!').processed
+      user_photo[:image] = rails_representation_url(thumbnail, only_path: true)
+      
       user_photo[:collectors] = photo.collectors.collect{|collect| collect.slice(:id, :username)}
       user_photos.push(user_photo)
     end
 
     return user_photos
-    
+
   end
 end
