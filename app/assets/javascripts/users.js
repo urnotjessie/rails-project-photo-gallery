e247c476cc19bc86ef55dfb6a9b9a205d53c237c
urnotjessie <zg2220@columbia.edu>
2018-10-21 10:21:54 -0400

    create image tag for image

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,7 +12,9 @@ function loadPhotos(photos) {
   debugger
   var photoCards = "";
   photos.forEach(function(photo) {
-    photoCards += '<div class="col-md-4 card">' + photo["id"] + ' - ' + photo["caption"] + '</div>';
+    photoCards += '<div class="col-md-4 card">' + photo["id"] + ' - ' + photo["caption"] +
+    '<image src="' + photo["image"] + '" class="img-thumbnail card-img-top"/>' +
+    '</div>';
       });
   $("#show-user-photos").html(photoCards);
 }
