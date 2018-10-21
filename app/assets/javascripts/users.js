1c693e4f697b1c09e7da701a4a33a7166828c3ba
urnotjessie <zg2220@columbia.edu>
2018-10-21 18:01:47 -0400

    add control flow for delete photo or add to my collection a tag

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 function userListeners() {
   var user_id = parseInt($('.show-user').attr('id'));
   var current_user = $('li[data-user-id]').data('user-id');
-
+  debugger
   $.get("/users/" + user_id + ".json", function(data) {
     photos = data.photos;
     loadPhotos(photos, user_id, current_user);
@@ -12,15 +12,22 @@ function loadPhotos(photos, user_id, current_user) {
   var photoCards = "";
 
   photos.forEach(function(photo) {
+
     photoCards += '<div class="col-md-4 card">' +
     '<image src="' + photo["image"] + '" class="img-thumbnail card-img-top"/><br>' +
     '<div class="card-body">' +
     photo["caption"] +
     '</div>';
 
-    if (user_id === current_user) {
-      photoCards += '<a href="#">Update caption</a>' +
+    if(user_id === current_user) {
+      photoCards += '<a href="#">Delete photo</a>' + ' | ' +
+      '<a href="#">Update caption</a>' +
       '</div>';
+    } else if(typeof current_user != "undefined") {
+      photoCards += '<a href="#">Add to my collection</a>' +
+      '</div>';
+    } else {
+      photoCards += '</div>'
     }
 
   });
