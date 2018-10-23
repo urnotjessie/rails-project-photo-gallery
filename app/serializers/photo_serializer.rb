9873bd722c848a4cce1f7821ed421f9dff4cde81
urnotjessie <zg2220@columbia.edu>
2018-10-22 21:18:28 -0400

    add collectors attributes to photos api

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,6 +1,6 @@
 class PhotoSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :caption, :image
+  attributes :id, :caption, :created_at, :image, :collectors
 
   def image
     thumbnail = object.image.variant(resize:'300x300!').processed
@@ -9,4 +9,16 @@ class PhotoSerializer < ActiveModel::Serializer
       image: rails_blob_path(object.image, only_path: true)
     }
   end
+
+  def collectors
+    collectors_data = []
+    
+    object.collectors.each do |collector|
+      attributes = {id: collector.id, username: collector.username}
+      collectors_data.push(attributes)
+    end
+
+    return collectors_data
+  end
+
 end
