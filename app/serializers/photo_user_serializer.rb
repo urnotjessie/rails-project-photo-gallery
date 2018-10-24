84a6f7450a11eaed01b7f5ba2d87197d3366ac11
urnotjessie <zg2220@columbia.edu>
2018-10-24 18:44:32 -0400

    add creator attribute to photo_user_serializer

--- app/serializers/photo_user_serializer.rb
+++ app/serializers/photo_user_serializer.rb
@@ -1,5 +1,13 @@
 class PhotoUserSerializer < ActiveModel::Serializer
-  
+  attributes :creator
+
+  def creator
+    {
+      id: Photo.find(object.collected_photo.id).user.id,
+      username: Photo.find(object.collected_photo.id).user.username
+    }
+  end
+
   belongs_to :collector, class_name: 'User', serializer: UserSerializer
   belongs_to :collected_photo, class_name: 'Photo', serializer: PhotoSerializer
 end
