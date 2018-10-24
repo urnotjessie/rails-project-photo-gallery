f22610e9d687c2419332fb450e89f5ec46340256
urnotjessie <zg2220@columbia.edu>
2018-10-24 18:47:32 -0400

    append image info on photo_user show page

---
+++ app/assets/javascripts/photos.js
@@ -0,0 +1,19 @@
+function photoListeners() {
+
+  var collection_id = $('.show-photo').data('collection-id');
+  var user_id = $('.show-photo').data('user-id');
+
+  // create image element for photo show page
+  $.get("/users/" + user_id + "/photo_users/" + collection_id + ".json", function(data) {
+
+      var photo = data.collected_photo;
+      var creator = data.creator;
+
+      var imageTag = '<image src="' + photo.image["thumbnail"] + '" class="img-thumbnail card-img-top"/><br>' +
+      '<div class="card-body">' +
+      '<br>[<strong>' + creator.username + '</strong> - ' + photo.caption + ']' + '  ' +
+      photo.created_at + '<br></div>';
+
+      $(".show-photo-image").append(imageTag);
+    });
+}
