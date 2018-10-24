e7364bf962b8c424030402a93c981379370bbf2b
urnotjessie <zg2220@columbia.edu>
2018-10-24 13:46:20 -0400

    delete id attribute in PhotoUserSerializer to prevent confusion

--- app/serializers/photo_user_serializer.rb
+++ app/serializers/photo_user_serializer.rb
@@ -1,6 +1,5 @@
 class PhotoUserSerializer < ActiveModel::Serializer
-  attributes :id
-
+  
   belongs_to :collector, class_name: 'User', serializer: UserSerializer
   belongs_to :collected_photo, class_name: 'Photo', serializer: PhotoSerializer
 end
