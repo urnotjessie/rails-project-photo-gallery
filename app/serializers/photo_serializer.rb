eee3d825b299a6a2a07d515f20654370fa207433
urnotjessie <zg2220@columbia.edu>
2018-11-01 10:42:07 -0400

    add next_id attribute in photo json

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,10 +1,21 @@
 class PhotoSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  
-  attributes :id, :caption, :created_at, :image, :total_collectors
+
+  attributes :id, :caption, :next_id, :created_at, :image, :total_collectors
 
   belongs_to :user, serializer: UserSerializer
   has_many :collectors, class_name: 'User', through: :photo_users, serializer: UserSerializer
+
+  def next_id
+    photos = object.user.photos
+    photos.each_with_index do |photo,index|
+      if (object.id == photo.id) && (index+2 <= photos.length)
+        return photos[index+1].id
+      elsif index+2 == photos.length
+        return false
+      end
+    end
+  end
 
   def created_at
     return object.created_at.strftime("%Y-%m-%d")
