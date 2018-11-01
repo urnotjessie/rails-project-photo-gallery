1dca1469431ec2cc6df093d0e89ac8a3eb5ff411
urnotjessie <zg2220@columbia.edu>
2018-11-01 10:38:23 -0400

    complete render next photo function

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,30 +1,10 @@
 function photoListeners() {
-  var photo_id = $('.show-photo').data('photo-id');
-  var user_id = $('.show-photo').data('user-id');
+  var photoId = $('.show-photo').data('photo-id');
+  var userId = $('.show-photo').data('user-id');
 
-  // create image element for photo show page
-  $.get("/photos/" + photo_id + ".json", function(data) {
-
-      var photo = data.image;
-      var caption = data.caption;
-      var user = data.user;
-      var creation = data.created_at;
-
-      var imageTag = '<image src="' + photo["thumbnail"] + '" class="img-thumbnail card-img-top"/><br>' +
-      '<div class="card-body">' +
-      '<br>[<strong>' + user.username + '</strong> - <span id="caption">' + caption + '</span>]' + '  ' +
-      creation + '<br>' +
-      '<p><a class="update-label">Update caption</a></p></div>';
-
-      $(".show-photo-image").append(imageTag);
-    });
-
-  // eventlistener for clicking Update caption on photo show page
-  $(".show-photo-image").on("click", '.update-label', function() {
-
-    // var labelForm = '<form class="edit_photo" type="hidden" method="post" data-remote="true" action="/photos/' + photo_id + '">' +
-    var labelForm = '<form id="caption_form" action="/photos/' + photo_id + '">' +
-    // '<input type="hidden" name="_method" value="patch">' +
+  // append update-caption form on click
+  $(".update-caption").on("click", function() {
+    var labelForm = '<form id="caption_form" action="/photos/' + photoId + '">' +
     '<input type="text" name="photo[caption]" id="photo_caption" value="' + $("#caption")[0].innerHTML + '"/>' +
     '<input type="submit" value="submit" id="submit" /></form>';
     $(".caption-form").append(labelForm);
@@ -50,80 +30,38 @@ function photoListeners() {
         $("form").hide();
       }
     })
-    // $.post(url, function(){
-    //   debugger
-    // });
-
-    // update.done(function(data){
-    //   debugger
-    // })
-    // product.done(function(data) {
-    //   var product = data;
-    //   $("#productName").text(product["name"]);
-    //   $("#productPrice").text(product["price"]);
-    //   $("#productDescription").text(product["description"]);
-    // });
   })
 
-  // click next to go to next photo show page
+  //
   $(".js-next").on("click", function() {
-    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    $.get("/products/" + nextId + ".json", function(data) {
-      var product = data;
-      $(".productName").text(product["name"]);
-      $(".productPrice").text(product["price"]);
-      $(".productDescription").text(product["description"]);
-      $(".productInventory").text(product["inventory"]);
-      // re-set the id to current on the link
-      $(".js-next").attr("data-id", product["id"]);
+    var currentId = $(".js-next").attr("data-id");
+
+    $.get("/photos/" + currentId + ".json", function(data) {
+      var photo = data;
+      var nextPhoto = photo.next_id;
+
+      $(".js-next").attr("data-id", nextPhoto);
+
+      showPhoto(nextPhoto);
     });
   });
-  // $('#caption_form').submit(function(event) {
-  //   event.preventDefault();
-  //   debugger
-  //   var values = $(this).serialize();
-  //   var product = $.post('/products', values);
-  //
-  //   product.done(function(data) {
-  //     var product = data;
-  //     $("#productName").text(product["name"]);
-  //     $("#productPrice").text(product["price"]);
-  //     $("#productDescription").text(product["description"]);
-  //   });
-  //
-  // });
 }
 
+function showPhoto(photoId) {
+  // create image element for photo show page
+  $.get("/photos/" + photoId + ".json", function(data) {
 
+      var photo = data.image;
+      var caption = data.caption;
+      var user = data.user;
+      var creation = data.created_at;
 
-// function photoListeners() {
-//   var collection_id = $('.show-photo').data('collection-id');
-//   var user_id = $('.show-photo').data('user-id');
-//
-//   // create image element for photo show page
-//   $.get("/users/" + user_id + "/photo_users/" + collection_id + ".json", function(data) {
-//
-//       var photo = data.collected_photo;
-//       var creator = data.creator;
-//       var label = data.label;
-//
-//       var imageTag = '<image src="' + photo.image["thumbnail"] + '" class="img-thumbnail card-img-top"/><br>' +
-//       '<div class="card-body">' +
-//       '<br>[<strong>' + creator.username + '</strong> - ' + photo.caption + ']' + '  ' +
-//       photo.created_at + '<br>' +
-//       '<p>label: <span id="label">' + label + '</span>' + ' - ' +
-//       '<a class="update-label">Update label</a></p></div>';
-//
-//       $(".show-photo-image").append(imageTag);
-//     });
-//
-//     // eventlistener for clicking Update label on photo show page
-//     $(".show-photo-image").on("click", '.update-label', function() {
-//
-//       var labelForm = '<form type="hidden" method="POST" action="#">' +
-//       '<input type="text" value="' + $("#label")[0].innerHTML + '"/>' +
-//       '<input type="submit" value="submit" id="submit"/></form>';
-//       $(".show-photo-image").append(labelForm);
-//     });
-//
-// }
+      var imageTag = '<image src="' + photo["thumbnail"] + '" class="img-thumbnail card-img-top"/><br>' +
+      '<div class="card-body">' +
+      '<br>[<strong>' + user.username + '</strong> - <span id="caption">' + caption + '</span>]' + '  ' +
+      creation + '<br>' +
+      '</div>';
+
+      $(".show-photo-image").empty().append(imageTag);
+    });
+}
